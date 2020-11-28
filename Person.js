b1be0face7bea979b4429791d68e4504639d045d
ShravyaHub <shravyak710@gmail.com>
2020-11-28 22:36:59 +0530

    ADDED: code to create an address book with first name, last name, address, city, state, zip, phone number, email

---
+++ Person.js
@@ -0,0 +1,30 @@
+class Person{
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString(){
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email; 
+    }
+}
+
+    person = new Person("Shravya", "K", "Bangalore", "Bangalore", "KA", 560065, 9591255544, "shravya@gmail.com");
+    console.log(person.toString());
+    
